2a19ed79d148160aa645f7857ad2ad6aebd0f8d1
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-04-20 23:54:18 +0530

    Added entities

--- src/main/java/dev/ankit/productservice/models/Category.java
+++ src/main/java/dev/ankit/productservice/models/Category.java
@@ -1,5 +1,6 @@
 package dev.ankit.productservice.models;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Category {
-    private Long id;
+@Entity
+public class Category extends BaseModel {
     private String title;
 }
--- src/main/java/dev/ankit/productservice/models/Product.java
+++ src/main/java/dev/ankit/productservice/models/Product.java
@@ -1,5 +1,8 @@
 package dev.ankit.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
-    private Long id;
+@Entity
+public class Product extends BaseModel {
     private String title;
     private String description;
     private double price;
     private String imageUrl;
+
+    @ManyToOne
     private Category category;
 }
